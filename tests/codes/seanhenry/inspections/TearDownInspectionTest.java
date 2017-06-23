4e9771fc18b965b327b12320f7151c4b263390d6
Sean Henry <hello@seanhenry.codes>
2017-06-23 12:35:07 -0400

    Adds less hacky solution for setting up tests

--- tests/codes/seanhenry/inspections/TearDownInspectionTest.java
+++ tests/codes/seanhenry/inspections/TearDownInspectionTest.java
@@ -1,28 +1,22 @@
 package codes.seanhenry.inspections;
 
-import codes.seanhenry.helpers.MyHeavyIdeaTestFixtureImpl;
+import codes.seanhenry.helpers.ImportProjectTestFixture;
 import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.testFramework.PlatformTestCase;
 import com.intellij.testFramework.fixtures.CodeInsightTestFixture;
-import com.intellij.testFramework.fixtures.impl.CodeInsightTestFixtureImpl;
+import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory;
 import com.intellij.testFramework.fixtures.impl.TempDirTestFixtureImpl;
 import org.junit.Assert;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
+public class TearDownInspectionTest extends PlatformTestCase {
 
-public class TearDownInspectionTests extends PlatformTestCase {
-
-  private final String dataPath = "/Users/sean/source/plugins/community/TearDown/testData/TestProject";
+  private static final String dataPath = "/Users/sean/source/plugins/community/TearDown/testData/TestProject";
   private CodeInsightTestFixture myFixture;
 
   @Override
@@ -33,7 +27,6 @@ public class TearDownInspectionTests extends PlatformTestCase {
     } finally {
       myFixture = null;
       try {
-
         super.tearDown();
       } catch (Throwable ignored) {}
     }
@@ -43,16 +36,11 @@ public class TearDownInspectionTests extends PlatformTestCase {
   protected void setUpProject() throws Exception {
     super.setUpProject();
 
-    VfsRootAccess.allowRootAccess("/Applications/Xcode-8.3.2.app/Contents");
-
     TempDirTestFixtureImpl tempDirTestFixture = new TempDirTestFixtureImpl();
-    String projectDir = tempDirTestFixture.getTempDirPath();
-    copyFolder(new File(dataPath), new File(projectDir));
-
-    String name = getClass().getName() + "." + getName();
-    myFixture = new CodeInsightTestFixtureImpl(new MyHeavyIdeaTestFixtureImpl(name, projectDir + "/TestProject.xcodeproj"), tempDirTestFixture);
-
-    myFixture.setUp();
+    myFixture = IdeaTestFixtureFactory.getFixtureFactory().createCodeInsightFixture(new ImportProjectTestFixture(dataPath, "TestProject.xcodeproj", tempDirTestFixture), tempDirTestFixture);
+    try {
+      myFixture.setUp();
+    } catch (Exception ignored) {}
     myFixture.setTestDataPath(dataPath);
     myFixture.enableInspections(new TearDownInspection());
   }
@@ -130,38 +118,5 @@ public class TearDownInspectionTests extends PlatformTestCase {
     VirtualFile file = psiFile.getVirtualFile();
     myFixture.configureFromExistingVirtualFile(file);
     return psiFile;
-  }
-
-  private void copyFolder(File sourceFolder, File destinationFolder) throws IOException {
-    //Check if sourceFolder is a directory or file
-    //If sourceFolder is file; then copy the file directly to new location
-    if (sourceFolder.isDirectory())
-    {
-      //Verify if destinationFolder is already present; If not then create it
-      if (!destinationFolder.exists())
-      {
-        destinationFolder.mkdir();
-        //System.out.println("Directory created :: " + destinationFolder);
-      }
-
-      //Get all files from source directory
-      String files[] = sourceFolder.list();
-
-      //Iterate over all files and copy them to destinationFolder one by one
-      for (String file : files)
-      {
-        File srcFile = new File(sourceFolder, file);
-        File destFile = new File(destinationFolder, file);
-
-        //Recursive function call
-        copyFolder(srcFile, destFile);
-      }
-    }
-    else
-    {
-      //Copy the file content from one place to another
-      Files.copy(sourceFolder.toPath(), destinationFolder.toPath(), StandardCopyOption.REPLACE_EXISTING);
-      //System.out.println("File copied :: " + destinationFolder);
-    }
   }
 }
