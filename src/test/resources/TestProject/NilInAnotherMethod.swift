f1d0b636bed0e387cd5c0076b5aeba68a6310de4
Sean Henry <hello@seanhenry.codes>
2018-06-03 18:35:58 +1000

    Removes protocol extension functionality

--- src/test/resources/TestProject/NilInAnotherMethod.swift
+++ src/test/resources/TestProject/NilInAnotherMethod.swift
@@ -1,31 +1,17 @@
 import XCTest
 
-protocol AProtocol: class {
-    var alreadyNilledInProtocolExtension: String? { get set }
-    func setToNilInProtocolExtension()
-}
-
-extension AProtocol where Self: XCTestCase {
-
-    func setToNilInProtocolExtension() {
-        alreadyNilledInProtocolExtension = nil
-    }
-}
-
-class <weak_warning descr="Not all properties are set to nil in the tear down.">NilInAnotherMethod<caret></weak_warning>: XCTestCase, AProtocol {
+class <weak_warning descr="Not all properties are set to nil in the tear down.">NilInAnotherMethod<caret></weak_warning>: XCTestCase {
 
     var nilledOutsideTearDown: String?
     var nilledInTheTearDown: String?
     var alreadyNilledInMethod: String?
     var alreadyNilledInExtension: String?
-    var alreadyNilledInProtocolExtension: String?
     var alreadyNilledInSelfMethod: String?
 
     override func tearDown() {
 
         setToNil()
         setToNilInExtension()
-        setToNilInProtocolExtension()
         self.setToNilUsingSelf()
         nilledInTheTearDown = nil
         super.tearDown()
--- src/test/resources/TestProject/NilInAnotherMethod_expected.swift
+++ src/test/resources/TestProject/NilInAnotherMethod_expected.swift
@@ -1,31 +1,17 @@
 import XCTest
 
-protocol AProtocol: class {
-    var alreadyNilledInProtocolExtension: String? { get set }
-    func setToNilInProtocolExtension()
-}
-
-extension AProtocol where Self: XCTestCase {
-
-    func setToNilInProtocolExtension() {
-        alreadyNilledInProtocolExtension = nil
-    }
-}
-
-class NilInAnotherMethod: XCTestCase, AProtocol {
+class NilInAnotherMethod: XCTestCase {
 
     var nilledOutsideTearDown: String?
     var nilledInTheTearDown: String?
     var alreadyNilledInMethod: String?
     var alreadyNilledInExtension: String?
-    var alreadyNilledInProtocolExtension: String?
     var alreadyNilledInSelfMethod: String?
 
     override func tearDown() {
 
         setToNil()
         setToNilInExtension()
-        setToNilInProtocolExtension()
         self.setToNilUsingSelf()
         nilledInTheTearDown = nil
         nilledOutsideTearDown = nil
